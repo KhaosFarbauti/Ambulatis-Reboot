99b97644adc7983b3f4734c5f73c3c3ff2195998
Khaos Farbauti Ibn Oblivion <propserv@hotmail.com>
2016-03-04 09:54:29 +0100

    Initial commit

---
+++ _define.php
@@ -0,0 +1,22 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of ambulatis, a theme for Dotclear.
+#
+# 2012 - annso - contact@as-i-am.fr
+#
+# Copyright (c) 2003-2010 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */		"Ambulatis Reboot",
+	/* Description*/	"A mobile theme",
+	/* Author */		"Original By Annso, Hacked by Khaos Farbauti Ibn Oblivion",
+	/* Version */		'2.0'
+);
+?>
